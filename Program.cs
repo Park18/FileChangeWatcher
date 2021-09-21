0254c6d1b342c11c53aeb9655beee757fb8f614a
Park18 <sowoomi35@gmai.com>
2021-09-21 22:57:41 +0900

    updated Program.cs
    * created ChangeWatcher instance

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace FileWatcher
     {
         static void Main(string[] args)
         {
+            FileWatcher.ChangeWatcher changeWatcher = new ChangeWatcher();
+            changeWatcher.run();
         }
     }
 }
