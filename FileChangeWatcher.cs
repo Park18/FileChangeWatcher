14a319ebe683a58cb60db606f127bc9e5e4b8fba
Park18 <sowoomi35@gmail.com>
2021-09-27 18:07:47 +0900

    made FileChangeWatcher.cs
    FileChangeWatcher is ScoreSystem's kernel

---
+++ FileChangeWatcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FileChangeWatcher.ScoreSystem;
+
+namespace FileChangeWatcher
+{
+    class FileChangeWatcher
+    {
+        private string path = @"C:\Users\sowoo\Desktop\FileChangeDataset";
+
+        private S1 s1 = new S1();
+        private S2 s2 = new S2();
+
+        public void Run()
+        {
+            var filesystemWatcher = new FileSystemWatcher(path);
+
+            filesystemWatcher.NotifyFilter = NotifyFilters.Attributes
+                                            | NotifyFilters.CreationTime
+                                            | NotifyFilters.DirectoryName
+                                            | NotifyFilters.FileName
+                                            | NotifyFilters.LastAccess
+                                            | NotifyFilters.LastWrite
+                                            | NotifyFilters.Security
+                                            | NotifyFilters.Size;
+
+            filesystemWatcher.Changed += OnChanged;
+            filesystemWatcher.Created += OnCreated;
+            filesystemWatcher.Deleted += OnDeleted;
+            filesystemWatcher.Renamed += OnRenamed;
+            filesystemWatcher.Error += OnError;
+
+            filesystemWatcher.IncludeSubdirectories = true;
+            filesystemWatcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            //Console.WriteLine($"Changed: {e.FullPath} - time: {DateTime.Now.ToString()}");
+            Console.WriteLine($"Changed: {e.Name}");
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath} - time: {DateTime.Now.ToString()}";
+            Console.WriteLine(value);
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e) =>
+            Console.WriteLine($"Deleted: {e.FullPath} - time: {DateTime.Now.ToString()}");
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed: - time: {DateTime.Now.ToString()}");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+        }
+
+        private void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private void PrintException(Exception ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+    }
+}
