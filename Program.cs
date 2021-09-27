c13079bce8f3003b4e63f803f2cdc19a76778a30
Park18 <sowoomi35@gmail.com>
2021-09-27 18:08:18 +0900

    testing code

--- Program.cs
+++ Program.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileWatcher
+using FileChangeWatcher.ScoreSystem;
+using System.Threading;
+
+namespace FileChangeWatcher
 {
     class Program
     {
         static void Main(string[] args)
         {
-            FileWatcher.ChangeWatcher changeWatcher = new ChangeWatcher();
-            changeWatcher.run();
+            // S1 테스트
+            S1 s1 = new S1();
+            s1.Run();
+
+            Thread.Sleep(1000 * 3);
+            s1.Run();
+
+            // S2 테스트
+            // S2 s2 = new S2();
+            // s2.Run();
         }
     }
 }
