fadfa7a69d0da05738cedf998adb4123ded6bc16
Park18 <sowoomi35@gmail.com>
2021-09-29 19:40:30 +0900

    updated S1.cs
    add comment at class S1
    delete var count

--- ScoreSystem/S1.cs
+++ ScoreSystem/S1.cs
@@ -8,11 +8,16 @@ using System.Threading.Tasks;
 
 namespace FileChangeWatcher.ScoreSystem
 {
+    /// <summary>
+    /// S1 점수 클래스
+    /// </summary>
+    /// <remarks>
+    /// 전체 파일중에 변경된 파일의 개수에 따른 점수
+    /// </remarks>
     class S1 : AbstractScoreSystem
     {
         private static int WaitingTime = 1000 * 10;
 
-        private int count = 0;
         private bool isFirstChange = true;  // 첫 번째 변화인지 확인하는 플래그
         private Thread thread;
         private Timer timer;
