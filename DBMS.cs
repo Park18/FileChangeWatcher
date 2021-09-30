0a7a7cf90d2e1b666e9d8955b707eb050cf5aee7
Park18 <sowoomi35@gmail.com>
2021-09-30 16:27:51 +0900

    Created DBMS.cs

---
+++ DBMS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileChangeWatcher
+{
+    class DBMS
+    {
+        private static int _totalFileNumbers = 0;
+        private static List<string> _changeFileList = new List<string>();
+
+        public int TotalFileNumbers
+        {
+            get { return _totalFileNumbers; }
+        }
+
+        public List<string> ChangeFileList
+        {
+            get { return _changeFileList; }
+        }
+
+        public bool Init()
+        {
+            return true;
+        }
+
+        public void ResetChangeFileList() =>
+            _changeFileList.Clear();
+    }
+}
