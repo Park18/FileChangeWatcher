282b86254639593517b97dba1128033ccbd9b99f
Park18 <sowoomi35@gmail.com>
2021-09-30 17:54:59 +0900

    updated FileChangeWatcher.cs
    add timer function of S1 was transferred to FileChangeWatcher.
    add comment
    add var DMBS

--- FileChangeWatcher.cs
+++ FileChangeWatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,34 @@ namespace FileChangeWatcher
 {
     class FileChangeWatcher
     {
-        private string path = @"D:\Code\Capstone\FileWatcher\FileChangeDataset";
-        private int totalNumberOfFiles = 0;
+        /// <summary>
+        /// Filesystem 관련
+        /// </summary>
+        private string path = @"D:\Code\Capstone\FileChangeWatcher\FileChangeDataset";
 
+        /// <summary>
+        /// 타이머 관련
+        /// </summary>
+        private bool isFirstChange = true;
+        private const int WaitingTime = 1000 * 10;
+        private Thread thread;
+        private Timer timer;
+
+        /// <summary>
+        /// ScoreSystem 관련
+        /// </summary>
         private S1 s1 = new S1();
         private S2 s2 = new S2();
+
+        /// <summary>
+        /// DBMS 관련
+        /// </summary>
+        private DBMS dbms = new DBMS();
+
+        public FileChangeWatcher()
+        {
+            dbms.Init();
+        }
 
         public void Run()
         {
@@ -51,20 +75,20 @@ namespace FileChangeWatcher
             }
             //Console.WriteLine($"Changed: {e.FullPath} - time: {DateTime.Now.ToString()}");
             Console.WriteLine($"Changed: {e.Name}");
-            s1.Run();
+            this.CheckWork();
         }
 
         private void OnCreated(object sender, FileSystemEventArgs e)
         {
             string value = $"Created: {e.FullPath} - time: {DateTime.Now.ToString()}";
             Console.WriteLine(value);
-            s1.Run();
+            this.CheckWork();
         }
 
         private void OnDeleted(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine($"Deleted: {e.FullPath} - time: {DateTime.Now.ToString()}");
-            s1.Run();
+            this.CheckWork();
         }
 
         private void OnRenamed(object sender, RenamedEventArgs e)
@@ -73,7 +97,7 @@ namespace FileChangeWatcher
             Console.WriteLine($"    Old: {e.OldFullPath}");
             Console.WriteLine($"    New: {e.FullPath}");
 
-            s1.Run();
+            this.CheckWork();
         }
 
         private void OnError(object sender, ErrorEventArgs e) =>
@@ -89,6 +113,58 @@ namespace FileChangeWatcher
                 Console.WriteLine();
                 PrintException(ex.InnerException);
             }
+        }
+
+        /// <summary>
+        /// 연속된 작업인지 확인하고 타이머를 성정하는 메소드
+        /// </summary>
+        private void CheckWork()
+        {
+            if (this.isFirstChange)
+                this.isFirstChange = false;
+
+            if (thread == null)
+                thread = new Thread(SetTimer);
+
+            else if (thread.ThreadState != ThreadState.Stopped || thread.ThreadState != ThreadState.Aborted)
+            {
+                thread.Abort();
+                this.timer.Dispose();
+                thread = new Thread(SetTimer);
+            }
+
+            thread.Start();
+        }
+
+        /// <summary>
+        /// 연속된 작업의 끝을 알기 위한 타이머의 시간을 설정하는 메소드
+        /// </summary>
+        private void SetTimer()
+        {
+            // Test Code
+            Console.WriteLine("Thread{0}: begin", Thread.CurrentThread.ManagedThreadId);
+
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+            this.timer = new Timer(this.TimerRun, autoResetEvent, WaitingTime, 0);
+
+
+            autoResetEvent.WaitOne();
+            this.timer.Dispose();
+
+            // Test Code
+            Console.WriteLine("Thread{0}: end", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        /// <summary>
+        /// 연속된 작업의 끝을 알기 위한 타이머가 끝났을 때 작동하는 메소드
+        /// </summary>
+        private void TimerRun(Object stateInfo)
+        {
+            this.isFirstChange = true;
+            s1.Run();
+
+            AutoResetEvent autoResetEvent = (AutoResetEvent)stateInfo;
+            autoResetEvent.Set();
         }
     }
 }
