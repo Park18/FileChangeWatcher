7d7f8980c03b8f28dd18a26a6ad207bfb09c8bc6
Park18 <sowoomi35@gmail.com>
2021-09-30 17:56:27 +0900

    updated S1.cs
    delete timer function of S1 was transferred to FileChangeWatcher.

--- ScoreSystem/S1.cs
+++ ScoreSystem/S1.cs
@@ -27,51 +27,7 @@ namespace FileChangeWatcher.ScoreSystem
         /// </summary>
         public override void Run()
         {
-            if (this.isFirstChange)
-                this.isFirstChange = false;
-
-            if (thread == null)
-                thread = new Thread(SetTimer);
-
-            else if (thread.ThreadState != ThreadState.Stopped || thread.ThreadState != ThreadState.Aborted)
-            {
-                thread.Abort();
-                this.timer.Dispose();
-                thread = new Thread(SetTimer);
-            }
-
-            thread.Start();
-        }
-
-        /// <summary>
-        /// 타이머의 시간을 설정하는 메소드
-        /// </summary>
-        private void SetTimer()
-        {
-            // Test Code
-            // Console.WriteLine("Thread{0}: begin", Thread.CurrentThread.ManagedThreadId);
-
-            AutoResetEvent autoResetEvent = new AutoResetEvent(false);
-            this.timer = new Timer(this.TimerRun, autoResetEvent, WaitingTime, 0);
-
-
-            autoResetEvent.WaitOne();
-            this.timer.Dispose();
-            
-            // Test Code
-            // Console.WriteLine("Thread{0}: end", Thread.CurrentThread.ManagedThreadId);
-        }
-
-        /// <summary>
-        /// 설정된 시간에 작동하는 타이머 메소드
-        /// </summary>
-        private void TimerRun(Object stateInfo)
-        {
-            this.isFirstChange = true;
             this.Calculate();
-
-            AutoResetEvent autoResetEvent = (AutoResetEvent)stateInfo;
-            autoResetEvent.Set();
         }
 
         /// <summary>
@@ -81,6 +37,8 @@ namespace FileChangeWatcher.ScoreSystem
         {
             this._isCompleteCalculate = true;
             Console.WriteLine("Calculate(): Begin");
+
+
         }
     }
 }
