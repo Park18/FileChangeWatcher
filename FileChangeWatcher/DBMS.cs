79da1cc18e132134f2f7c5634056042ca8020a79
Park18 <sowoomi35@gmail.com>
2021-10-04 00:34:32 +0900

    update DBMS.cs
    add comment
    add AddChangeFile method
    add struct Data

--- FileChangeWatcher/DBMS.cs
+++ FileChangeWatcher/DBMS.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace FileChangeWatcher
 {
+    struct Data
+    {
+        string path;
+        string fuzzy;
+        string shannon;
+    }
+
     class DBMS
     {
         private static int _totalFileNumbers = 0;
@@ -21,12 +28,20 @@ namespace FileChangeWatcher
             get { return _changeFileList; }
         }
 
+        /// <summary>
+        /// DB를 초기화하는 메소드
+        /// </summary>
+        /// <returns>DB 초기화 실패 여부</returns>
         public bool Init()
         {
+            this.ResetChangeFileList();
             return true;
         }
 
         public void ResetChangeFileList() =>
             _changeFileList.Clear();
+
+        public void AddChangeFile(string path)
+            => _changeFileList.Add(path);
     }
 }
